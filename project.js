f819deb551da5d3f7b4ee09ddae7adf4bab486f5
ghosharnab969 <ghosharnab969@gmail.com>
2021-03-25 01:50:07 +0530

    project completed!

--- project.js
+++ project.js
@@ -1,3 +1,28 @@
-let a = 100;
-let b = 3;
-console.log(a + b);
+let input = prompt("What would you like to do?")
+const todos = ['wash clothes', 'study web dev', 'watch movie'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === "list") {
+        console.log("********************************");
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}:${todos[i]}`);
+        }
+        console.log("********************************");
+    }
+    else if (input === "new") {
+        const newTodo = prompt('what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list.`);
+    }
+    else if (input === 'delete') {
+        const index = parseInt(prompt("which index to be deleted?"));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`task "${deleted}" has been deleted!`);
+        }
+        else {
+            console.log("Sorry! Unknown Index...");
+        }
+    }
+    input = prompt('what would you like to do?');
+}
+console.log("Ok! you quit.");
